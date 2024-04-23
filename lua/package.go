d4db6f17bed0854dfdf5b19551641fdb3590e1cf
wzy <i@example.com>
2024-04-23 14:34:25 +0800

    移除无用的代码

--- lua/package.go
+++
@@ -1,9 +0,0 @@
-// GopherLua: VM and compiler for Lua in Go
-package lua
-
-const (
-	PackageName      = "GopherLua"
-	PackageVersion   = "0.1"
-	PackageAuthors   = "Yusuke Inuzuka"
-	PackageCopyRight = PackageName + " " + PackageVersion + " Copyright (C) 2015 -2017 " + PackageAuthors
-)
